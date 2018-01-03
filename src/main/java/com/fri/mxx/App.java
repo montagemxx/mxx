29b846518926b531e5bf533f72d8b55e68ac6dd8
montagemxx <montage_mxx@yahoo.com>
2018-01-02 20:19:10 -0500

    first commit

---
+++ src/main/java/com/fri/mxx/App.java
@@ -0,0 +1,13 @@
+package com.fri.mxx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
