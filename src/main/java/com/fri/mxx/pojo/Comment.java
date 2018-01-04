0f69288321b4fd9d67bec76c53ed0b79d405f62a
montagemxx <montage_mxx@yahoo.com>
2018-01-03 21:59:30 -0500

    missing getter and setter

--- src/main/java/com/fri/mxx/pojo/Comment.java
+++ src/main/java/com/fri/mxx/pojo/Comment.java
@@ -3,4 +3,16 @@ package com.fri.mxx.pojo;
 public class Comment {
 	private String email;
 	private String commentText;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCommentText() {
+		return commentText;
+	}
+	public void setCommentText(String commentText) {
+		this.commentText = commentText;
+	}
 }
